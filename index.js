845e13364a2f328d3b063168cf50d550ab26ff58
Haziq Luqman <hazeek@Haziqs-MacBook-Air.local>
2023-12-22 10:32:22 +0800

    added some changes in mongo connection

--- index.js
+++ index.js
@@ -11,36 +11,36 @@ app.use(express.static("public")); // initialize public folder as static
 
 // SESSION SECRET MIDDLEWARE //
 app.use(
-  session({
-    secret: "somerandomstring",
-    resave: false,
-    saveUninitialized: false,
-  })
+	session({
+		secret: "somerandomstring",
+		resave: false,
+		saveUninitialized: false,
+	})
 );
 
 const port = process.env.PORT || 3000;
 
+mongoose.set("strictQuery", false);
+
 ////////////////// MONGOOSE/MONGO DB ACTIVITIES
-const mongoUri = `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST_NAME}/${process.env.MONGO_DB_NAME}`;
+const mongoUri = process.env.MONGO_URI;
 
 mongoose.connect(mongoUri, { useNewUrlParser: true }, () => {
-  console.log(
-    "Establishing connection with Mongo DB: " + process.env.MONGO_DB_NAME
-  );
+	console.log("Establishing connection with Mongo DB");
 });
 
 const db = mongoose.connection;
 
 db.on("connected", () => {
-  console.log("Successfully established connection");
+	console.log("Successfully established connection");
 });
 
 db.on("error", (err) => {
-  console.log("Unable to establish connection: " + err.message);
+	console.log("Unable to establish connection: " + err.message);
 });
 
 db.on("disconnected", () => {
-  console.log("Disconnected from Mongo DB");
+	console.log("Disconnected from Mongo DB");
 });
 
 app.use(methodOverride("_method"));
@@ -56,11 +56,11 @@ app.use("/user", userController);
 app.use("/sessions", sessionController);
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    currentUser: req.session.currentUser,
-  });
+	res.render("index.ejs", {
+		currentUser: req.session.currentUser,
+	});
 });
 
 app.listen(port, () => {
-  console.log("App is listening on port: " + port);
+	console.log("App is listening on port: " + port);
 });
